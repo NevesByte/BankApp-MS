7c9fc7eb6c6a059d3d8264659850d74dda77a087
Victor <victorneves596@gmail.com>
2026-03-30 15:18:26 -0300

    corrigindo bugs do emial sender

--- email-sender-ms/src/main/java/com/email_ms/email_sender_ms/EmailSenderMsApplication.java
+++ email-sender-ms/src/main/java/com/email_ms/email_sender_ms/EmailSenderMsApplication.java
@@ -1,8 +1,10 @@
 package com.email_ms.email_sender_ms;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableRabbit
 @SpringBootApplication
 public class EmailSenderMsApplication {
 
--- email-sender-ms/src/main/java/com/email_ms/email_sender_ms/config/RabbitMQConfig.java
+++ email-sender-ms/src/main/java/com/email_ms/email_sender_ms/config/RabbitMQConfig.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
@@ -29,6 +30,7 @@ public class RabbitMQConfig {
     public ObjectMapper objectMapper() {
         ObjectMapper mapper = new ObjectMapper();
         mapper.registerModule(new JavaTimeModule());
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         return mapper;
     }
 }
